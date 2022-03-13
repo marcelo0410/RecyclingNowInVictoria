73f4563841f5185207da4eeb46481a3e728d3b12
marcelo0410 <cs95401156@gmail.com>
2022-03-13 21:47:05 +0800

    Type of bins layout: change layout, text and onclick function

--- src/pages/GarbageClassification/wastephoto.js
+++ src/pages/GarbageClassification/wastephoto.js
@@ -8,37 +8,72 @@ export default class wastemouseenter extends Component {
       photoArray: [ 
         {
           id:1,
-          title:'paper',
-          imageUrl:'wastephoto/plasticbottle/plasticbottle1.jpg',
-          genre:'recyclebin/yellowbin.png',
+          title:'plasticbottle',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/plastic-bottles.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/recycle1.png?raw=true',
           disable:true
         },
         {
           id:2,
-          title:'paper',
-          imageUrl:'wastephoto/plasticbottle/plasticbottle1.jpg',
-          genre:'recyclebin/yellowbin.png',
+          title:'glassbottle',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/glass-bottle.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/glass1.png?raw=true',
           disable:true
         },
         {
           id:3,
-          title:'paper',
-          imageUrl:'wastephoto/plasticbottle/plasticbottle1.jpg',
-          genre:'recyclebin/yellowbin.png',
+          title:'magazines',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/magazines.jpeg?raw=true?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/recycle1.png?raw=true',
           disable:true
         },
         {
           id:4,
-          title:'paper',
-          imageUrl:'wastephoto/plasticbottle/plasticbottle1.jpg',
-          genre:'recyclebin/yellowbin.png',
+          title:'papercups',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/paper-cups.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/recycle1.png?raw=true',
           disable:true
         },
         {
           id:5,
-          title:'paper',
-          imageUrl:'wastephoto/plasticbottle/plasticbottle1.jpg',
-          genre:'recyclebin/yellowbin.png',
+          title:'aluminumcans',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/aluminum-can.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/recycle1.png?raw=true',
+          disable:true
+        },
+        {
+          id:6,
+          title:'fruitpeels',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/fruit-peels.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/organics1.png?raw=true',
+          disable:true
+        },
+        {
+          id:7,
+          title:'polystyrene',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/polystyrene.jpg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/garbage1.png?raw=true',
+          disable:true
+        },
+        {
+          id:8,
+          title:'cardboard',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/cardboard.jpeg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/recycle1.png?raw=true',
+          disable:true
+        },
+        {
+          id:9,
+          title:'plasticbags',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/plastic-bags.jpg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/garbage1.png?raw=true',
+          disable:true
+        },
+        {
+          id:10,
+          title:'plasticwrap',
+          imageUrl:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/wastephoto/plastic-wrap.jpg?raw=true',
+          genre:'https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/eee05eeb9ea6665b81a55d7ab4b0db13fdfb5eb9/recyclebin/garbage1.png?raw=true',
           disable:true
         }
       ]
@@ -69,13 +104,13 @@ export default class wastemouseenter extends Component {
   render() {
     return (
       <div>
-          <div className='mt-5 mb-2'>Common genre of garbage</div>
-          <div style={{display:"flex", justifyContent:"center", flexWrap:"wrap", width:"900px"}}>
+          <div className='mt-5 mb-2' style={{textAlign:"left", fontSize:"20px", marginLeft:"40px", fontWeight:"bold"}}>Common search</div>
+          <div style={{display:"flex", justifyContent:"center", flexWrap:"wrap", width:"900px", marginLeft:"35px", marginBottom:"30px"}}>
               {
                 this.state.photoArray.map((item, index) =>(
                   <div key={item.id}>
                     <img 
-                      src={item.disable? (require('../../assets/images/' + item.imageUrl)):(require('../../assets/images/' + item.genre))}
+                      src={item.disable? (item.imageUrl):(item.genre)}
                       className=""
                       style={{width:"180px", height:"100px"}}
                       alt={item.title}
--- src/pages/recyclebin.js
+++ src/pages/recyclebin.js
@@ -6,50 +6,126 @@ import GreenBinImage from "../assets/images/recyclebin/greenbin.png"
 import './style.css'
 
 export default class recyclebin extends Component {
+  photoPrefix = "https://github.com/zx0217/RecyclingNowInVictoriaIMG/blob/362fed1a53791d09d0c764204067f1d35c1ca225/Bins/"
+  photoPostfix = "?raw=true"
+  state={
+    disable:[true, true, true, true]
+  }
+  binArray= [
+    {
+      id:1,
+      title:"General waste",
+      desc:"This is a red bin to put general wastes, including:",
+      genre:['General rubbish', 'Nappies (wrapped or bagged)','Polystyrene and foam', 'Meat trays', 'Plastic bags and soft plastics', 'Window glass'],
+      imgUrl:"garbage2.png",
+      hoverUrl:"garbage1.png"
+    },
+    {
+      id:2,
+      title:"Recycling",
+      desc:"This is a yellow bin to put recyclable wastes, including:",
+      genre:['Plastic bottles and containers', 'Paper and cardboard','Aluminium cans', 'Steel cans', 'Delivery boxes'],
+      imgUrl:"recycle2.png",
+      hoverUrl:"recycle1.png"
+    },
+    {
+      id:3,
+      title:"organic wastes",
+      desc:"This is a green bin to put organic wastes, including:",
+      genre:['Food wastes', 'Garden soil','Fruit and vegetable peels'],
+      imgUrl:"organics2.png",
+      hoverUrl:"organics1.png"
+    },
+    {
+      id:4,
+      title:"Glass container",
+      desc:"(colour may be vary based on location) </br> This is a blue bin to put the glass containers, including:",
+      genre:['Glass bottles', 'Glass containers for food'],
+      imgUrl:"glass2.png",
+      hoverUrl:"glass.png"
+    },
+  ]
+
+  getId = (e)=>{
+    let tempDisable = [...this.state.disable]
+    tempDisable[e.currentTarget.id-1] = !tempDisable[e.currentTarget.id-1]
+    this.setState({
+      disable:tempDisable
+    })
+  }
+
   render() {
     return (
       <div class="card-group mt-5">
         <div class="card">
-          <div className='card-photo-bgc-red'>
-            <img src={RedBinImage} class="card-img-top img-same-wh" alt="..."/>
+          <div className='card-photo-bgc-red' id="1" onClick={this.getId}>
+            <img src={this.state.disable[0]? (this.photoPrefix+this.binArray[0].imgUrl+this.photoPostfix):(this.photoPrefix+this.binArray[0].hoverUrl+this.photoPostfix)} class="card-img-top img-same-wh" alt="General waste"/>
           </div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title" style={{fontWeight:"bold", marginBottom:"10px"}}>General waste</h5>
+            <p class="card-text">
+              <div>This is a red bin to put general wastes, including:</div>
+              <ul style={{listStyle:"initial", textAlign:"center", display: "table", marginTop:"10px"}}>
+                <li style={{textAlign:"left"}}>General rubbish</li>
+                <li style={{textAlign:"left"}}>Nappies (wrapped or bagged)</li>
+                <li style={{textAlign:"left"}}>Polystyrene and foam </li>
+                <li style={{textAlign:"left"}}>Meat trays</li>
+                <li style={{textAlign:"left"}}>Window glass</li>
+              </ul>
+            </p>
           </div>
         </div>
         <div class="card">
-          <div className='card-photo-bgc-yellow'>
-            <img src={YellowBinImage} class="card-img-top img-same-wh" alt="..."/>
+          <div className='card-photo-bgc-yellow' id="2" onClick={this.getId}>
+            <img src={this.state.disable[1]? (this.photoPrefix+this.binArray[1].imgUrl+this.photoPostfix):(this.photoPrefix+this.binArray[1].hoverUrl+this.photoPostfix)} class="card-img-top img-same-wh" alt="..."/>
           </div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title" style={{fontWeight:"bold", marginBottom:"10px"}}>Recycling</h5>
+            <p class="card-text">
+              <div>This is a yellow bin to put recyclable wastes, including:</div>
+              <ul style={{listStyle:"initial", textAlign:"center", display: "table", marginTop:"10px"}}>
+                <li style={{textAlign:"left"}}>Plastic bottles and containers </li>
+                <li style={{textAlign:"left"}}>Paper and cardboard</li>
+                <li style={{textAlign:"left"}}>Aluminium cans</li>
+                <li style={{textAlign:"left"}}>Steel cans</li>
+                <li style={{textAlign:"left"}}>Delivery boxes</li>
+              </ul>
+            </p>
           </div>
         </div>
         <div class="card">
-          <div className='card-photo-bgc-green'>
-            <img src={GreenBinImage} class="card-img-top img-same-wh" alt="..."/>
+          <div className='card-photo-bgc-green' id="3" onClick={this.getId}>
+            <img src={this.state.disable[2]? (this.photoPrefix+this.binArray[2].imgUrl+this.photoPostfix):(this.photoPrefix+this.binArray[2].hoverUrl+this.photoPostfix)} class="card-img-top img-same-wh" alt="..."/>
           </div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title" style={{fontWeight:"bold", marginBottom:"10px"}}>Organic wastes</h5>
+            <p class="card-text">
+              <div>This is a green bin to put organic wastes, including:</div>
+              <ul style={{listStyle:"initial", textAlign:"center", display: "table", marginTop:"10px"}}>
+                <li style={{textAlign:"left"}}>Food wastes</li>
+                <li style={{textAlign:"left"}}>Garden soil</li>
+                <li style={{textAlign:"left"}}>Fruit and vegetable peels</li>
+              </ul>
+            </p>
           </div>
         </div>
         <div class="card">
-          <div className='card-photo-bgc-blue'>
-            <img src={BlueBinImage} class="card-img-top img-same-wh" alt="..."/>
+          <div className='card-photo-bgc-blue' id="4" onClick={this.getId}>
+            <img src={this.state.disable[3]? (this.photoPrefix+this.binArray[3].imgUrl+this.photoPostfix):(this.photoPrefix+this.binArray[3].hoverUrl+this.photoPostfix)} class="card-img-top img-same-wh" alt="..."/>
           </div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title" style={{fontWeight:"bold", marginBottom:"10px"}}>Glass container</h5>
+            <p class="card-text">
+              <div>(colour may be vary based on location)<br/>This is a blue bin to put the glass containers, including:</div>
+              <ul style={{listStyle:"initial", textAlign:"center", display: "table", marginTop:"10px"}}>
+                <li style={{textAlign:"left"}}>Glass bottles</li>
+                <li style={{textAlign:"left"}}>Glass containers for food</li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
+
     )
   }
 }
