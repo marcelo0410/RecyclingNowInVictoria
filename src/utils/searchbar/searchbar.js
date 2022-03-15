8fdc76f466d3a895f79d0bd3704890a93a7787b8
marcelo0410 <cs95401156@gmail.com>
2022-03-15 18:31:34 +0800

    Search function: support enter key to search

--- src/utils/searchbar/searchbar.js
+++ src/utils/searchbar/searchbar.js
@@ -58,12 +58,19 @@ export default class searchbar extends Component {
     }
   }
 
+  handleEnterKey = (evt) =>{
+    console.log(evt)
+    if(evt.key === 'Enter'){
+      this.searchGenre()
+    }
+  }
+
   render() {
     return (
       <div>
         <div class="wrapper">
           <div class="searchBar">
-            <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Enter waste's catagory or key word" value={this.state.searchText} onChange={this.updateInputValue}/>
+            <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Enter waste's catagory or key word" value={this.state.searchText} onChange={this.updateInputValue} onKeyUp={this.handleEnterKey}/>
             <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit" onClick={this.searchGenre}>
               <svg style={{width:"24px", height:"24px"}} viewBox="0 0 24 24"><path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
               </svg>
